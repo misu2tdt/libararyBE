b0c8b4e8d880861d1355373c457c8ce9a6cb7c0a
Tien <tdtien7a1@gmail.com>
2025-06-25 14:43:49 +0700

    add and complete author controller and route

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,3 +1,4 @@
+const { parse } = require('dotenv');
 const prisma = require('../utils/prismaClient');
 
 const createAuthor = async(req, res) =>{
@@ -30,28 +31,45 @@ const getAuthors = async(req, res) =>{
 
 const getAuthorById = async(req, res) =>{
     try{
+        const id = parse(req.params.id);
+        const author = await prisma.author.findUnique( { where: {id} } );
+        if(!author){
+            res.status(404).json({message: 'Author Not Found'})
+        }
+        res.json(author)
 
-
-    }catch{
-        
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message: 'Internal Server Error'})
     }
 };
 
 const updateAuthor = async(req, res) =>{
     try{
+        const id = parse(req.params.id);
+        const {name, bio} = req.body;
+        const updateAuthor = await prisma.author.update({
+            where: {id},
+            data: {name, bio}
+        });
+        res.json(updateAuthor);
 
-
-    }catch{
-        
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message: 'Internal Server Error'})
     }
 };
 
 const deleteAuthor = async(req, res) =>{
     try{
-
-
-    }catch{
-        
+        const id = parse(req.params.id);
+        const deleteAuthor = await prisma.author.delete({
+            where: {id}
+        })
+        res.json({message: 'Author deleted'});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message: 'Internal Server Error'})
     }
 };
 
--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const router = express.router();
+const{
+    createAuthor,
+    getAuthors,
+    getAuthorById,
+    updateAuthor,
+    deleteAuthor
+} = require('../controllers/authorController');
+
+router.post('/', createAuthor);
+router.get('/', getAuthors);
+router.get('/:id', getAuthorById);
+router.put('/:id', updateAuthor);
+router.delete('/:id', deleteAuthor);
+
+module.exports = router;
